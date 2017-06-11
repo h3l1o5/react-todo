ff1f326a5126582741008e37974d5b07ac543769
martysun <martian206@gmail.com>
2017-06-11 22:13:26 +0800

    react-todo init commit

--- src/index.jsx
+++ src/index.jsx
@@ -3,38 +3,11 @@ import ReactDOM from 'react-dom'
 import './index.css'
 
 class App extends Component {
-  constructor() {
-    super()
-    this.state = {
-      greetingMessage: 'Your name?',
-      user: ''
-    }
-  }
-
-  onChange = (e) => {
-    this.setState({user: e.target.value})
-  }
-
-  handleSubmit = (e) => {
-    e.preventDefault()
-    if (this.state.user !== '') {
-      this.setState({
-        greetingMessage: 'Hello, '+this.state.user+'!',
-        user: ''
-      })
-    } else {
-      this.setState({greetingMessage: 'No name?'})
-    }
-  }
 
   render() {
     return (
       <div className='main'>
-        <h1>{this.state.greetingMessage}</h1>
-        <form onSubmit={this.handleSubmit}>
-          <input onChange={this.onChange} placeholder="your name here" value={this.state.user}/>
-          <button>SEND</button>
-        </form>
+        <h1>HELLO WORLD</h1>
       </div>
     )
   }
